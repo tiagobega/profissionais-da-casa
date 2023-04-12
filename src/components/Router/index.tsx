f0e459e61b319f016f5b8a52104c22dc7c43fd3d
tiagobega <tiagovbega@outlook.com>
2023-04-12 00:19:13 -0300

    merge das rotas

--- src/components/Router/index.tsx
+++ src/components/Router/index.tsx
@@ -1,14 +1,124 @@
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useUser } from "contexts/User";
+import React, { Suspense } from "react";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import Layout from "components/Layout";
 
 const HomePage = React.lazy(() => import("pages/home"));
 
 const Router = () => (
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<HomePage />} />
+      <Route path={"/"} element={<Layout />}>
+        {/* //OPEN ROUTES */}
+        <Route path={"/"} element={<PublicRoute element={<HomePage />} />} />
+        <Route
+          path={"/not-found"}
+          element={<PublicRoute element={<HomePage />} />}
+        />
+        <Route
+          path={"/register/customer"}
+          element={<PublicRoute element={<HomePage />} />}
+        />
+        <Route
+          path={"/register/professional"}
+          element={<PublicRoute element={<HomePage />} />}
+        />
+        <Route
+          path={"/register/professional"}
+          element={<PublicRoute element={<HomePage />} />}
+        />
+        <Route path={"/faq"} element={<PublicRoute element={<HomePage />} />} />
+        <Route
+          path={"/terms-conditions"}
+          element={<PublicRoute element={<HomePage />} />}
+        />
+        <Route
+          path={"/catalog"}
+          element={<PublicRoute element={<HomePage />} />}
+        />
+        <Route
+          path={"/professional/:id"}
+          element={<PublicRoute element={<HomePage />} />}
+        />
+        <Route
+          path={"/project/:id"}
+          element={<PublicRoute element={<HomePage />} />}
+        />
+        {/* //CUSTOMER/PROFESSIONAL ROUTES */}
+        <Route
+          path={"/my-projects/"}
+          element={<PublicRoute element={<HomePage />} />}
+        />
+        <Route
+          path={"/project-details/"}
+          element={<PublicRoute element={<HomePage />} />}
+        />
+        <Route
+          path={"/review/:id"}
+          element={<PublicRoute element={<HomePage />} />}
+        />
+        //ADMIN ROUTES
+        <Route
+          path={"/admin/"}
+          element={<PublicRoute element={<HomePage />} />}
+        />
+        <Route
+          path={"/admin/reviews"}
+          element={<PublicRoute element={<HomePage />} />}
+        />
+        <Route
+          path={"/admin/review/:id"}
+          element={<PublicRoute element={<HomePage />} />}
+        />
+        <Route
+          path={"/admin/professionals-management"}
+          element={<PublicRoute element={<HomePage />} />}
+        />
+        <Route
+          path={"/admin/professionals-management/:id"}
+          element={<PublicRoute element={<HomePage />} />}
+        />
+        <Route
+          path={"/admin/professionals-management/:id/projects"}
+          element={<PublicRoute element={<HomePage />} />}
+        />
+      </Route>
+
+      <Route index element={<PublicRoute element={<HomePage />} />} />
     </Routes>
   </BrowserRouter>
 );
+
+const SuspenseComponent = ({ children }: { children: React.ReactNode }) => (
+  <Suspense fallback={<>Loading...</>}>{children}</Suspense>
+);
+
+const PrivateRoute = ({
+  element,
+  redirectTo,
+  admin = false,
+}: {
+  element: React.ReactElement;
+  redirectTo?: string;
+  admin?: boolean;
+}) => {
+  const { logged } = useUser();
+  const useIsAdmin = true;
+
+  if (!logged && redirectTo) {
+    return <Navigate to={redirectTo} />;
+  }
+
+  if (admin && !useIsAdmin) {
+    redirectTo ??= "/";
+    return <Navigate to={redirectTo} />;
+  }
+
+  return <SuspenseComponent>{element}</SuspenseComponent>;
+};
+
+const PublicRoute = ({ element }: { element: React.ReactElement }) => {
+  return <SuspenseComponent>{element}</SuspenseComponent>;
+};
 
 export default Router;
