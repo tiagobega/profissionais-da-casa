b621e3b1bcda189907769cdb660a97f1af199c24
tiagobega <tiagovbega@outlook.com>
2023-04-11 23:38:01 -0300

    correção nas rotas e aplicação de plugin vite-tsconfig-paths

--- src/components/Router/index.tsx
+++ src/components/Router/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const HomePage = React.lazy(() => import("pages/home"));
 
 const Router = () => (
   <BrowserRouter>
-    <Route element={<HomePage />} />
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+    </Routes>
   </BrowserRouter>
 );
 
