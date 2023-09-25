8830fa566fe7c2ecaf415fe0bc86080084acdeab
tiagobega <tiagovbega@outlook.com>
2023-09-25 18:23:53 -0300

    ajustes da ultima semana

--- src/constants/formationLevels.ts
+++ src/constants/formationLevels.ts
@@ -3,7 +3,9 @@ export const formationLevels = [
   // { name: "Médio Incompleto", value: "Médio Incompleto" },
   // { name: "Médio Completo", value: "Médio Completo" },
   // { name: "Superior Incompleto", value: "Superior Incompleto" },
+  { name: "Técnico", value: "Técnico" },
   { name: "Superior Completo", value: "Superior Completo" },
+  { name: "Pós Graduado", value: "Pós Graduado" },
   { name: "Mestrado", value: "Mestrado" },
   { name: "Doutorado", value: "Doutorado" },
 ];
