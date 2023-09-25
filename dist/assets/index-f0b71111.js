b3300bd58a50943cb38dd60fb21422511179e729
tiagobega <tiagovbega@outlook.com>
2023-09-25 17:57:26 -0300

    build 2

--- dist/assets/index-f0b71111.js
+++ dist/assets/index-f0b71111.js
@@ -1,4 +1,4 @@
-import{s as i,C as n,F as t,Z as s,u as c,j as o,a as e,G as r,B as d}from"./index-a1bef350.js";const h=i.section`
+import{s as i,C as n,F as t,Z as s,u as c,j as o,a as e,G as r,B as d}from"./index-3a790462.js";const h=i.section`
   ${({theme:a})=>n`
     width: 100%;
     display: flex;
