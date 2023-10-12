80235395446c3fdb9c6e99949ad39442059d6821
João Avelino <joaolavelino@gmail.com>
2023-10-12 14:16:08 -0300

    card image

--- src/pages/adm-professional-profile/styles.ts
+++ src/pages/adm-professional-profile/styles.ts
@@ -5,7 +5,6 @@ export const Photo = styled.section`
   ${({ theme }) => css`
     width: 170px;
     height: 170px;
-    object-fit: cover;
     overflow: hidden;
     border-radius: 50%;
     background-color: black;
@@ -13,6 +12,7 @@ export const Photo = styled.section`
     img {
       width: 100%;
       height: 100%;
+      object-fit: cover;
     }
   `}
 `;
