536ed1b45194ee9ad76346b648847906a3431144
João Avelino <joaolavelino@gmail.com>
2023-10-13 14:03:21 -0300

    edit professional profils

--- src/components/Forms/FormEditProfile/validation.ts
+++ src/components/Forms/FormEditProfile/validation.ts
@@ -5,8 +5,6 @@ import * as z from "zod";
 export const editProfileSchema = z.object({
   name: z.string().min(1, formErrors.ERROR_REQUIRED),
   description: z.string().min(1, formErrors.ERROR_REQUIRED),
-  profilePicture: fileRefine({ required: false }),
-  backgroundPicture: fileRefine({ size: 5, required: false }),
-  states: z.array(z.string()),
   onlineAppointment: z.boolean(),
+  tags: z.array(z.string()).min(1, "Selecione ao menos uma categoria"),
 });
