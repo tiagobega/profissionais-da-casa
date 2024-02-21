783361c45ad95c4b11889d809ed6c61a9144c06f
João Avelino <joaolavelino@gmail.com>
2024-02-20 21:04:40 -0300

    not found page

--- src/components/NotFound/styles.ts
+++ src/components/NotFound/styles.ts
@@ -1,11 +1,43 @@
-import styled, { css } from 'styled-components'
-import { FlexBox } from 'components/FlexBox'
+import styled, { css } from "styled-components";
+import { FlexBox } from "components/FlexBox";
+import { media } from "styles/utils";
 
 export const TextContainer = styled(FlexBox)`
   ${({ theme }) => css`
-    width: 30rem;
+    width: auto;
+
     h2 {
-      font-size: 2rem;
+      font-size: 1.5rem;
+    }
+    ${media.lg`
+      width: 30rem;
+        h2 {
+          font-size: 2rem;
+        }
+    `}
+  `}
+`;
+export const Container = styled(FlexBox)`
+  ${({ theme }) => css`
+    padding-inline: 2rem;
+    img {
+      display: none;
+      ${media.lg`
+      display: block;
+    `}
     }
   `}
-`
+`;
+
+export const ContentContainer = styled(FlexBox)`
+  ${({ theme }) => css`
+    flex-direction: column;
+    align-items: center;
+    gap: 2rem;
+    ${media.lg`
+      flex-direction: row;
+      gap: 0;
+      align-items:flex-end;
+    `}
+  `}
+`;
